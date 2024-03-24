ca06f597617a650466360fce7ebdfd8d2e9efa46
Durgesh Kumar <142595053+DurgeshKumar143@users.noreply.github.com>
2024-03-24 13:29:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -21,16 +21,18 @@ process.on("uncaughtException",(err)=>{
 })
 
 
-
-
-connectDB().then(()=>{
-    app.listen(process.env.PORT || 800,()=>{
-        console.log(`Server is running port ${process.env.PORT}`)
+ app.listen(process.env.PORT || 800,()=>{
+         console.log(`Server is running port ${process.env.PORT}`)
     })
-}).catch((error)=>{
-    console.log("MongoDb Connection Error ",error)
-    process.exit()
-})
+
+// connectDB().then(()=>{
+//     app.listen(process.env.PORT || 800,()=>{
+//         console.log(`Server is running port ${process.env.PORT}`)
+//     })
+// }).catch((error)=>{
+//     console.log("MongoDb Connection Error ",error)
+//     process.exit()
+// })
 
 
 
